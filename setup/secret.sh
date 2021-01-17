c747bd5a7b71733fec5882eda7cb9ee7040429f6
Xiaokang Guo <xg590@nyu.edu>
2021-01-17 18:24:43 -0500

    Update secret.sh

--- setup/secret.sh
+++ setup/secret.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 [ -d secret ] || mkdir secret
-openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout secret/server.key -out secret/server.crt -subj /CN=yzlab3.chem.nyu.edu
+[ 'foo'$1 = 'foo' ] && echo "You must provide a domain name" && exit
+openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout secret/server.key -out secret/server.crt -subj /CN=$1
 openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout secret/rover.key  -out secret/rover.crt -subj /CN=rover
-openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout secret/driver.key -out secret/driver.crt -subj /CN=driver 
+openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout secret/driver.key -out secret/driver.crt -subj /CN=driver
 cat secret/rover.crt secret/driver.crt > secret/clients.crt
 echo -e "Hal?ou\nWode*%^!()" > secret/basicAuth.conf
